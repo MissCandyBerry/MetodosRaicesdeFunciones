b050553493fac710bb78e7d2916db9d879f50392
Candy <dulcemariahernandezibarra@gmail.com>
2025-09-14 21:22:42 -0700

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Metodos_de_Raices_de_Funciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
